14939135f2df24934f6b3ce1d7e0ef9e28c4ffd9
sam <nyarkosamuelasante@gmail.com>
2021-07-07 00:41:52 +0000

    Added the pages to it and made some changes to the welcome page

--- lib/pages/WelcomePage.dart
+++ lib/pages/WelcomePage.dart
@@ -1,56 +1,91 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:shuttle_tracker/pages/signUpPage.dart';
 
-Widget welcomePage(BuildContext context) {
-  return Scaffold(
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
     resizeToAvoidBottomInset: false,
-    body: Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.yellowAccent, Colors.greenAccent])),
-      child: Column(
-        children: [
-          Container(
-              //logo,
-              ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 2 / 3,
-            child: Text("Shuttle Tracker"),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-              //Told hold detailed information about the app
-              ),
-          ElevatedButton(
-              onPressed: signUpClick, //navigate to the signUp page
-              child: Text(
-                "SignUp",
-                style: TextStyle(),
-              )),
-          ElevatedButton(
-            onPressed: loginClick, //navigate to the signUp page
-            child: Text(
-              "login",
-              style: TextStyle(),
+    body: SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.yellowAccent, Colors.greenAccent])),
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            Container(
+                //logo,
+                ),
+            SizedBox(
+              height: 10,
             ),
-          ),
-          Container(
-              //To hold the copyright symbol
-              )
-        ],
+            Container(
+              alignment: Alignment.center,
+              child: Text("Shuttle Tracker", style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                //Told hold detailed information about the app
+                ),
+                SizedBox(height: 450,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                padding: EdgeInsets.only(left: 50, right: 50, top: 20, bottom: 20),
+                shadowColor: Colors.grey,
+                primary: Colors.greenAccent
+              ),
+                onPressed: navigate, //navigate to the signUp page
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 18
+                  ),
+                )),
+                SizedBox(width: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                padding: EdgeInsets.only(left: 50, right: 50, top: 20, bottom: 20),
+                shadowColor: Colors.grey,
+                primary: Colors.white
+              ),
+              onPressed: navigate, //navigate to the signUp page
+              child: Text(
+                "Signup",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.greenAccent
+                ),
+              ),
+            ),
+              ],
+            ) ,     
+            Container(
+                //To hold the copyright symbol
+                )
+          ],
+        ),
       ),
     ),
   );
+    
+  }
 }
 
-void signUpClick() {}
+void navigate(){
 
-void loginClick() {}
+}
